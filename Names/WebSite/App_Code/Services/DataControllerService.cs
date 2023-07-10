34185ce2007372444d2c55c4ae68d12ae008646c
Nikolay Bobovnikov <b.nikolay@dilyasoft.pro>
2023-07-10 23:08:00 +0300

    Initial commit

---
+++ Names/WebSite/App_Code/Services/DataControllerService.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Web;
+using System.Web.Services;
+using System.Web.Script.Services;
+using TestCompany.Data;
+
+namespace TestCompany.Services
+{
+	public class DataControllerService
+    {
+        
+        public DataControllerService()
+        {
+        }
+        
+        public ViewPage GetPage(string controller, string view, PageRequest request)
+        {
+            return ControllerFactory.CreateDataController().GetPage(controller, view, request);
+        }
+        
+        public ActionResult[] ExecuteList(ActionArgs[] requests)
+        {
+            return ((DataControllerBase)(ControllerFactory.CreateDataController())).ExecuteList(requests);
+        }
+        
+        public ViewPage[] GetPageList(PageRequest[] requests)
+        {
+            return ((DataControllerBase)(ControllerFactory.CreateDataController())).GetPageList(requests);
+        }
+        
+        public object[] GetListOfValues(string controller, string view, DistinctValueRequest request)
+        {
+            return ControllerFactory.CreateDataController().GetListOfValues(controller, view, request);
+        }
+        
+        public ActionResult Execute(string controller, string view, ActionArgs args)
+        {
+            return ControllerFactory.CreateDataController().Execute(controller, view, args);
+        }
+        
+        public string[] GetCompletionList(string prefixText, int count, string contextKey)
+        {
+            return ControllerFactory.CreateAutoCompleteManager().GetCompletionList(prefixText, count, contextKey);
+        }
+        
+        public string GetSurvey(string survey)
+        {
+            return ControllerFactory.GetSurvey(survey);
+        }
+        
+        public object Login(string username, string password, bool createPersistentCookie)
+        {
+            return ApplicationServices.Login(username, password, createPersistentCookie);
+        }
+        
+        public void Logout()
+        {
+            ApplicationServices.Logout();
+        }
+        
+        public string[] Roles()
+        {
+            return ApplicationServices.Roles();
+        }
+        
+        public object Themes()
+        {
+            return ApplicationServices.Themes();
+        }
+    }
+}
---
+++ Names/WebSite/Site.aspx
@@ -0,0 +1,1 @@
+﻿<%@ Page Inherits="TestCompany.Handlers.Site" ValidateRequest="false" %>

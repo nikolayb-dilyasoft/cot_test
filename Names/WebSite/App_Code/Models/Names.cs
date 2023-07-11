84cd59b58a07472ad72bea0e41a323b2ad854638
Nikolay Bobovnikov <b.nikolay@dilyasoft.pro>
2023-07-11 03:31:14 +0300

    Add duplicate names filter rule
    (cherry picked from commit 7df9f92b7ea226627ee11123cf9dbe36c737e576)

---
+++ Names/WebSite/App_Code/Models/Names.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Data;
+using System.Collections.Generic;
+using System.Linq;
+using TestCompany.Data;
+
+namespace TestCompany.Models
+{
+	public partial class NamesModel : BusinessRulesObjectModel
+    {
+        
+        [System.Diagnostics.DebuggerBrowsable(System.Diagnostics.DebuggerBrowsableState.Never)]
+        private int? _iD;
+        
+        [System.Diagnostics.DebuggerBrowsable(System.Diagnostics.DebuggerBrowsableState.Never)]
+        private string _names;
+        
+        [System.Diagnostics.DebuggerBrowsable(System.Diagnostics.DebuggerBrowsableState.Never)]
+        private int? _type;
+        
+        public NamesModel()
+        {
+        }
+        
+        public NamesModel(BusinessRules r) : 
+                base(r)
+        {
+        }
+        
+        public int? ID
+        {
+            get
+            {
+                return _iD;
+            }
+            set
+            {
+                _iD = value;
+                UpdateFieldValue("ID", value);
+            }
+        }
+        
+        public string Names_
+        {
+            get
+            {
+                return _names;
+            }
+            set
+            {
+                _names = value;
+                UpdateFieldValue("Names_", value);
+            }
+        }
+        
+        public int? Type
+        {
+            get
+            {
+                return _type;
+            }
+            set
+            {
+                _type = value;
+                UpdateFieldValue("Type", value);
+            }
+        }
+    }
+}
---
+++ Names/WebSite/App_Code/Rules/Names.r100.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Data;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Web;
+using System.Web.Security;
+using TestCompany.Data;
+using TestCompany.Models;
+
+namespace TestCompany.Rules
+{
+	public partial class NamesBusinessRules : TestCompany.Data.BusinessRules
+    {
+        
+        /// <summary>
+        /// Rule "PreventDuplicateNames" implementation:
+        /// This method will execute in any view before an action
+        /// with a command name that matches "Insert".
+        /// </summary>
+        [Rule("r100")]
+        public void r100Implementation(NamesModel instance)
+        {
+           using (SqlText checkForDuplicate = new SqlText("SELECT COUNT(*) FROM [dbo].[Names] WHERE Names = @Name"))
+            {
+                checkForDuplicate.AddParameter("@Name", instance.Names_);
+                int count = Convert.ToInt32(checkForDuplicate.ExecuteScalar());
+                if (count > 0)
+                {
+                    PreventDefault(); // prevent the insert from happening
+                    Result.ShowMessage("Duplicate names are not allowed."); // show an error message to the user
+                }
+            }
+        }
+    }
+}
